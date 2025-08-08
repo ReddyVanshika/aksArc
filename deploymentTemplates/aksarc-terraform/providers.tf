6c80dd0c302dea193b7824fea672cbef5fe46314
ReddyVanshika <110912436+ReddyVanshika@users.noreply.github.com>
2025-08-08 12:56:41 -0400

    Update providers.tf

--- deploymentTemplates/aksarc-terraform/providers.tf
+++ deploymentTemplates/aksarc-terraform/providers.tf
@@ -1,21 +1,22 @@
-terraform {
-  required_version = ">=1.0"
-
-  required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
-    }
+terraform { 
+ required_version = "~> 1.5" 
+ required_providers { 
+   azapi = { 
+     source  = "azure/azapi" 
+     version = "~> 2.0" 
+   } 
+   azurerm = { 
+    source  = "hashicorp/azurerm" 
+    version = "~> 4.0" 
+   } 
   }
-}
-provider "azurerm" {
-  features {}
+ }
+
+ provider "azurerm" {
+ subscription_id = "<Subscription_ID>"
+ features { 
+  resource_group { 
+   prevent_deletion_if_contains_resources = false 
+  } 
+ } 
 }
